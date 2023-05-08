ebd0b976e91f5216fc54ae5de104018c43504708
Jahel <jahelcuadradomanchado@hotmail.com>
2023-05-08 13:14:32 +0200

    Screens creadas

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,6 +1,12 @@
 //TODO navegar entre pantallas con go_router 3: Esto es algo que podemos o hacer o no, es recomendado, ceramos este archivo en el que exportamos todas nuestras pantallas, esto nos ahorra tener muchas importaciones alli donde necesitemos importar todas nuestras pantallas, con que importemos este archivo es suficiente.
 
 
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screed.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
